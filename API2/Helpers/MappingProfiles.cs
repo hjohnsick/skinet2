9c379b09fe739cf0d15e7925e27fb4f6327926d1
heather <hjohnsick@yahoo.com>
2020-09-13 15:02:35 -0400

    End of section 4

---
+++ API2/Helpers/MappingProfiles.cs
@@ -0,0 +1,19 @@
+
+
+using API2.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API2.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+            .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+            .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+            .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+        }
+    }
+}
